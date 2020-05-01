ba31753b9b91b5accd0b1f83208fd5f8715e4e78
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-01 16:38:59 +0100

    coded server.js
    installed express from nodejs for the framework. bodyparser for using body method with req/res. mongodb to use non relational db, cors to bypass cors in web browser. helper createrouter to make optional paths.

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodbL//localhost:27017')
+.then((client)=>{
+    const db = client.db('movies_hub');
+    const moviesCollection = db.collection('movies');
+    const moviesRouter = createRouter(moviesCollection);
+    app.use('/api/movies',moviesRouter);
+}).catch(console.error)
+
+app.listen(3000,function(){
+    console.log(`listening on port ${this.address().port}`);
+}
