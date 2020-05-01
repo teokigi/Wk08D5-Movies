605cc347f14c2be304f6fb61c621ca8041ccc0fe
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-01 16:39:37 +0100

    reduced seeds to basic title and director
    some errors were flagging so data reduced to more simplifed title and director.

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,10 +1,9 @@
-use movies;
-
+use movies_hub;
 db.dropDatabase();
 
 db.movies.insertMany([
-    {title:"Gantz", director:"Shinsuke Sato", producer:"Takahiro Sato",runningTime:141,budget:"2.1 billion yen", boxOffice:"7.4 billion yen"},
-    {title:"Bleach", director:"Shinsuke Sato", boxOffice:"493,052,600 yen"},
-    {title:"Final Fantasy: Spirits Within", director:"Hironobu Sakaguchi", producer:["Chris Less","Jun Aida"],runningTime:106,budget:"137 million USD",boxOffice:"85.1 million USD"},
-    {title:"Final Fantasy: Advent Children", director:"Tetsuya Nomura", producer:["Yoshinori Kitase","Shinji Hashimoto","Ichiro Hazama"], runningTime:101}
+    {title:"Gantz", director:"Shinsuke Sato"},
+    {title:"Bleach", director:"Shinsuke Sato"},
+    {title:"Final Fantasy: Spirits Within", director:"Hironobu Sakaguchi"},
+    {title:"Final Fantasy: Advent Children", director:"Tetsuya Nomura"}
 ]);
