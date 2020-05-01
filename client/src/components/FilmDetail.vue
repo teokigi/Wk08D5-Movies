f538757c09efc90ce2f368dbc8e82dad8c66f590
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-01 18:17:12 +0100

    created film detail once show is selected

---
+++ client/src/components/FilmDetail.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div>
+      hello Film Detail World
+  </div>
+</template>
+
+<script>
+export default {
+    name:'film-detail',
+    props:['film']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
