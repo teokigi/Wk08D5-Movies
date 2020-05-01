1f77ff273b9cc0d7a06edd043f30088f72a6bdcd
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-01 17:20:47 +0100

    rating is age rating not popularity

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,8 +2,8 @@ use movies_hub;
 db.dropDatabase();
 
 db.movies.insertMany([
-    {title:"Gantz", Genre:"Action/Philosophical", rating:"10/10", release_date:"29jan11", runtime:"141 min"},
-    {title:"Bleach", Genre:"Action/Drama", rating:"5/10", release_date:"28jul18", runtime:"92 min"},
-    {title:"Final Fantasy: Spirits Within", Genre:"Action/Sci-Fi", rating:"4/10", release_date:"11jul01", runtime:"106 min"},
-    {title:"Final Fantasy: Advent Children", Genre:"Action/Adventure", rating:"9/10", release_date:"14sep05", runtime:"101 min"}
+    {title:"Gantz", Genre:"Action/Philosophical", rating:"30+", release_date:"29jan11", runtime:"141 min"},
+    {title:"Bleach", Genre:"Action/Drama", rating:"PG", release_date:"28jul18", runtime:"92 min"},
+    {title:"Final Fantasy: Spirits Within", Genre:"Action/Sci-Fi", rating:"30+", release_date:"11jul01", runtime:"106 min"},
+    {title:"Final Fantasy: Advent Children", Genre:"Action/Adventure", rating:"30+", release_date:"14sep05", runtime:"101 min"}
 ]);
