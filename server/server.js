5ee86832e9940f58e407a5f707317a02df3af029
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-01 16:41:06 +0100

    checkpoint
    tested mongodb compass to show database and relavant data after seeding. successful

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
-const cors = require('cors');
+const express = require('express');
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
