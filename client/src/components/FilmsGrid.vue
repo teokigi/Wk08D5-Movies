357f5e1dd9e86f75a111f9fd35facaee0a65814d
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-01 17:21:09 +0100

    fetch films

--- client/src/components/FilmsGrid.vue
+++ client/src/components/FilmsGrid.vue
@@ -7,17 +7,30 @@
 <script>
 import FilmGridItem from './FilmGridItem'
 export default {
-  data(){
-    return {
-      films: []
-    };
-  },
-  mounted(){
 
-  },
-  components: {
-    'film-grid-item': FilmGridItem
-  }
+    data(){
+        return {
+          films: []
+        };
+    },
+
+    mounted(){
+        this.getFilms();
+    },
+
+    components: {
+        'film-grid-item': FilmGridItem
+    },
+
+    methods:{
+        getFilms(){
+            fetch("http://localhost:3000/api/movies/")
+            .then(res=>res.json())
+            .then(resData => this.films = resData)
+
+        }
+    }
+
 }
 </script>
 
