86f2de01c05a44571150a0b76f7123b0e4c8b7f9
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-01 18:32:03 +0100

    changed shown information in component

--- client/src/components/FilmDetail.vue
+++ client/src/components/FilmDetail.vue
@@ -1,6 +1,11 @@
 <template lang="html">
   <div>
-      hello Film Detail World
+      <h1>{{film.title}} </h1>
+      <h2>Genre: {{film.Genre}}  </h2>
+      <h2>Rating: {{film.rating}}  </h2>
+      <h2>Release Date: {{film.release_date}}</h2>
+      <h2>Running time: {{film.runtime}}</h2>
+      <h2>Crew:<ul><li>Director: {{film.crew.director}}</li><li>Producer: {{film.crew.producer}}</li></ul></h2>
   </div>
 </template>
 
