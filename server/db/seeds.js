acf874469b6bdc638a8e3b77375e2f6d1227548f
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-01 17:30:43 +0100

    added crew property to seeds

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,8 +2,8 @@ use movies_hub;
 db.dropDatabase();
 
 db.movies.insertMany([
-    {title:"Gantz", Genre:"Action/Philosophical", rating:"30+", release_date:"29jan11", runtime:"141 min"},
-    {title:"Bleach", Genre:"Action/Drama", rating:"PG", release_date:"28jul18", runtime:"92 min"},
-    {title:"Final Fantasy: Spirits Within", Genre:"Action/Sci-Fi", rating:"30+", release_date:"11jul01", runtime:"106 min"},
-    {title:"Final Fantasy: Advent Children", Genre:"Action/Adventure", rating:"30+", release_date:"14sep05", runtime:"101 min"}
+    {title:"Gantz", Genre:"Action/Philosophical", rating:"30+", release_date:"29jan11", runtime:"141 min", crew:{director:"Shinsuke Sato", producer:"Takahiro Sato"}},
+    {title:"Bleach", Genre:"Action/Drama", rating:"PG", release_date:"28jul18", runtime:"92 min", crew:{director:"Shinsuke Sato", producer:"Warner Bros. Pictures"}},
+    {title:"Final Fantasy: Spirits Within", Genre:"Action/Sci-Fi", rating:"30+", release_date:"11jul01", runtime:"106 min", crew:{director:"Hironobu Sakaguchi", producer:"Chris Lee, Jun Aida"}},
+    {title:"Final Fantasy: Advent Children", Genre:"Action/Adventure", rating:"30+", release_date:"14sep05", runtime:"101 min", crew:{director:"Tetsuya Nomura", producer:"Yoshinori Kitase, Shinji Hashimoto, Ichiro Hazama"}}
 ]);
