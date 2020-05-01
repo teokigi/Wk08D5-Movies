96011213fa8df8c29b3cab4bd3bdeb0cf3a9b6bb
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-01 16:18:50 +0100

    +seeds
    made anime related movie list of seed data.

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,3 +1,10 @@
-use movies
+use movies;
 
 db.dropDatabase();
+
+db.movies.insertMany([
+    {title:"Gantz", director:"Shinsuke Sato", producer:"Takahiro Sato",runningTime:141,budget:"2.1 billion yen", boxOffice:"7.4 billion yen"},
+    {title:"Bleach", director:"Shinsuke Sato", boxOffice:"493,052,600 yen"},
+    {title:"Final Fantasy: Spirits Within", director:"Hironobu Sakaguchi", producer:["Chris Less","Jun Aida"],runningTime:106,budget:"137 million USD",boxOffice:"85.1 million USD"},
+    {title:"Final Fantasy: Advent Children", director:"Tetsuya Nomura", producer:["Yoshinori Kitase","Shinji Hashimoto","Ichiro Hazama"], runningTime:101}
+]);
